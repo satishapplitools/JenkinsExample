63ee58effb35e68bd48ce4a012f8190c820c76af
Satish Mallela <satish.mallela@applitools.com>
2019-07-26 09:13:20 +1000

    fourth commit

--- Jenkins/src/test/java/ExampleTest.java
+++ Jenkins/src/test/java/ExampleTest.java
@@ -76,7 +76,7 @@ public class ExampleTest {
         eyes.checkWindow("Home Page");
 
         //Adding an extra line of comment
-                
+        //Adding line
 
         // This will create a test with two test steps.
         driver.findElement(By.id("log-in")).click();
