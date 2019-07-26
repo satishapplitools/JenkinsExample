ddd004afc6fa56bfda0858cec11f1e29339b2236
Satish Mallela <satish.mallela@applitools.com>
2019-07-26 11:49:21 +1000

    Final Merge

--- Jenkins/src/test/java/ExampleTest.java
+++ Jenkins/src/test/java/ExampleTest.java
@@ -78,6 +78,7 @@ public class ExampleTest {
         //Adding an extra line of comment
         //Adding a comment
         //Adding an extra line
+        //Final Merge to check the status
 
         // This will create a test with two test steps.
         driver.findElement(By.id("log-in")).click();
